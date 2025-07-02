72be3dbf6f9a28cb547464f207224c53ca1ecf06
v.chichkin <v.chichkin@vk.team>
2025-07-03 01:30:30 +0300

    step 0 added docker

---
+++ image2text/main.py
@@ -0,0 +1,20 @@
+import time
+import requests
+import logging
+import torch
+
+from PIL import Image
+from transformers import AutoProcessor, Blip2ForConditionalGeneration
+
+
+def main():
+    print("placeholder code")
+
+    logging.info("succefully finished placeholder code")
+
+    while True:
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
