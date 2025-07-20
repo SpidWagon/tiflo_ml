73f4b536e6b343488ff18d5132991c124bbeac83
v.chichkin <v.chichkin@vk.team>
2025-07-20 16:57:10 +0300

    main commit

--- image2text/main.py
+++ image2text/main.py
@@ -1,23 +1,42 @@
+import os
+
 from fastapi import FastAPI
 from formats import *
-from utils.model_req import decode_base64_image, Model
+from utils.model_req import Model
 import uvicorn
+
+from models.model_download import download_model
+
 
 app = FastAPI()
 
-model_blip = Model()
+current_dir = os.getcwd()
+model_artifacts_dir = os.path.join("models", "model_artifacts")
+
+download_model(local_dir=os.path.join(current_dir, model_artifacts_dir))
+model_blip = Model(
+    translator_model=model_artifacts_dir,
+    translator_tokenizer=model_artifacts_dir
+)
 
 
 @app.post("/")
 def root(req: Req):
+    comments = []
+
     images = req.images
+    print("images aquired")
 
-    comment = model_blip.model_request(images)
-
+    for image in images:
+        image_decode = Model.decode_base64_image(image)
+        caption = model_blip.model_request(image_decode)
+        comments.append(caption)
+    print("captions done")
 
     return {
-        "comments": comment
+        "comments": comments
     }
+
 
 @app.get("/")
 def prikol():
